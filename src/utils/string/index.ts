31558078361971d18eaa43634b93d6a20859813b
Charles Lelis Braga <charles.braga.jf.cb@gmail.com>
2023-06-04 14:16:53 -0300

    feat: Testes da função de string

--- src/utils/string/index.ts
+++ src/utils/string/index.ts
@@ -1,6 +1,6 @@
 export const getIntByString = (str: string): number | null => {
   const numberRegex = /\d+/g;
-  const numbers = str.match(numberRegex)?.toString() ?? '';
+  const numbers = str.match(numberRegex)?.join('') ?? '';
   const parsed = parseInt(numbers);
   return isNaN(parsed) ? null : parsed;
 };
