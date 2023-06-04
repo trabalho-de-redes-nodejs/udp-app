8cf1b2cdf3d24de5b309d17695a792fbc5719077
Charles Lelis Braga <charles.braga.jf.cb@gmail.com>
2023-06-04 17:31:42 -0300

    clean: utils

--- src/client/utils/string/index.ts
+++
@@ -1,6 +0,0 @@
-export const getIntByString = (str: string): number | null => {
-  const numberRegex = /\d+/g;
-  const numbers = str.match(numberRegex)?.join('') ?? '';
-  const parsed = parseInt(numbers);
-  return isNaN(parsed) ? null : parsed;
-};
